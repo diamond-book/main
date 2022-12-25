8706a1f5aa5d683627fe4e15a915cbf0805ec4fd
Manav Gohel <2303manavgohel@gmail.com>
2022-12-26 02:09:57 +0530

    Home page is ready

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,1 +1,31 @@
 console.log("home.js running");
+
+
+let btn = document.getElementById("profile");
+let spans = document.getElementsByClassName("info");
+
+btn.onmouseenter = function () {
+    for (span of spans) {
+        span.classList.remove("profile-anim-reverse");
+        span.classList.add("profile-anim");
+    }
+}
+btn.onmouseleave = function () {
+    for (span of spans) {
+        span.classList.add("profile-anim-reverse");
+        span.classList.remove("profile-anim");
+
+    }
+};
+$(document).ready(function () {
+
+    $(".profile").slideDown(3000);
+
+    $(".profile").mouseover(function () {
+        $(".ex").slideDown();
+    })
+    $(".profile").mouseout(function () {
+        $(".ex").slideUp();
+    })
+
+});
