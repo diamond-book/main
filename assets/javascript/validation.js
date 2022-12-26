adbab6d2857ae30ea9eeeec1e64d81ea715d941c
Devendra <chavdadevendra03@gmail.com>
2022-12-26 12:18:57 +0530

    bugfixed

--- assets/javascript/validation.js
+++ assets/javascript/validation.js
@@ -1,26 +1,27 @@
 var phoneValidation = document.getElementsByClassName("phone-validation");
-phoneValidation.addEventListener("focusout", phoneValidationFun);
 
 
 var adharValidation = document.getElementsByClassName("adhar-validation");
-adharValidation.addEventListener("focusout", adharValidationFun);
 
 
 var nameValidation = document.getElementsByClassName("name-validation");
-nameValidation.addEventListener("focusout", nameValidationFun);
+phoneValidation.addEventListener("focusout", phoneValidationFun); 
 
 function ValidationFun() {
     if ((phoneValidation.value.length) != 10) {
+        document.getElementById('submit-validetion').action = '/';
         alert("Phone no. contain only 10 digit");
 
     }
     if ((adharValidation.value.length) != 12) {
         alert("adhar no. contain only 12 digit");
+        document.getElementById('submit-validetion').action = '/';
     }
     var s = nameValidation.value;
     for (var i = 0; i < s.length; ++i) {
         if ('0' < s[i] && s[i] < '9') {
             alert("name contain only alphabats");
+            document.getElementById('submit-validetion').action = '/';
         }
     }
 }
